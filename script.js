4e7c19e6047bf5058a886a3ba1abda7ed67108bf
Jananisibi <jananisibi2002@gmail.com>
2025-04-09 10:51:13 +0530

    completed

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   // Function to get the value of a cookie by name
   function getCookie(name) {
     let cookieArray = document.cookie.split('; ');
@@ -10,14 +10,17 @@ document.addEventListener('DOMContentLoaded', function() {
   function setCookie(name, value, daysToExpire) {
     let date = new Date();
     date.setTime(date.getTime() + daysToExpire * 24 * 60 * 60 * 1000);
-    document.cookie =
-      name + '=' + value + ';expires=' + date.toUTCString() + ';path=/';
+    document.cookie = name + '=' + value + ';expires=' + date.toUTCString() + ';path=/';
   }
 
-  // 1. Get the value of the 'count' cookie
-  // 2. If the cookie exists, increment the value and update the cookie
-  // 3. If the cookie does not exist, create it and set the value to 1
-  // 4. Display the count on the webpage
+  const count = getCookie('count');
 
-  // your code here
+  if (count) {
+        setCookie("count",parseInt(count)+1,7);
+  } else {
+    setCookie("count",1,7);
+  }
+
+
+  document.getElementById('countDisplay').textContent = getCookie("count");
 });
